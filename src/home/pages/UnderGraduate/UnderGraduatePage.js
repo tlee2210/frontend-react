4a53a546c0498562f3669fb03951787d9ac37ac5
monterdv <quangdaivi2303@gmail.com>
2024-01-22 17:06:14 +0700

    nothing

--- src/home/pages/UnderGraduate/UnderGraduatePage.js
+++ src/home/pages/UnderGraduate/UnderGraduatePage.js
@@ -4,7 +4,6 @@ import HeaderGraduate from "./components/Header/HeaderGraduate";
 import NavBarGraduate from "./components/NavBarGraduate/NavBarGraduate";
 
 function UnderGraduatePage() {
-<<<<<<< Updated upstream
   return (
     <div style={{ paddingTop: 150 }}>
       <ParticlesAuth>
@@ -13,17 +12,6 @@ function UnderGraduatePage() {
       </ParticlesAuth>
     </div>
   );
-=======
-    return (
-        <div style={{paddingTop:100}}>
-        <ParticlesAuth>
-            <HeaderGraduate/>
-            <NavBarGraduate/>
-        </ParticlesAuth>
-
-        </div>
-    )
->>>>>>> Stashed changes
 }
 
 export default UnderGraduatePage;
